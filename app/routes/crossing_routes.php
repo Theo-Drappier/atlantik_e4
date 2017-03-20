fe8c875ccc120072ae8813e59ece1338e2c4b0b3
Théo Drappier <theo.drappier85@gmail.com>
2017-03-20 16:17:29 +0100

    Commit array Crossings

--- app/routes/crossing_routes.php
+++ app/routes/crossing_routes.php
@@ -2,7 +2,8 @@
 
 $app->get('/crossing', function() use ($app)
 {
+    $crossings = $app['build.crossing']->findAll();
     return $app['twig']->render(
-        'crossing/crossing.html.twig', array('user' => $app['session']->get('currentUser'))
+        'crossing/crossing.html.twig', array('user' => $app['session']->get('currentUser'), 'crossings' => $crossings)
     );
 });
